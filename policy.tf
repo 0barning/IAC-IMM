a5f8604f5593bb1f31edcc3ff58788210396a7d1
0barning <olaf.barning@outlook.com>
2021-04-16 22:13:18 +0200

    Add IMM Policy

--- policy.tf
+++ policy.tf
@@ -2,16 +2,19 @@ data "intersight_organization_organization" "org" {
     name = var.organization
 }
 
-resource "intersight_hyperflex_vcenter_config_policy" "hyperflex_vcenter_config_policy1" {
-  hostname    = "vcenter.workload.local"
-  username    = "administrator@workload.local"
-  password    = var.password
-  data_center = "WLT"
-  sso_url     = ""
+
+
+resource "intersight_server_profile" "IMM-ESXi-01" {
+  name   = "IMM-ESXi-01"
+  action = "No-op"
+  target_platform = "FIAttached"
+  
+  tags {
+    key   = "deployment_type"
+    value = "terraform"
+  }
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.org.results[0].moid
   }
-  name = "HyperFlex_vCenter_Policy"
-  description = "Created by Terraform"
 }
