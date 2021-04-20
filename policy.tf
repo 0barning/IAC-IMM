13cb7e8480657be76eb3638d67c027a1a3e293ad
0barning <olaf.barning@outlook.com>
2021-04-20 13:08:53 +0200

    Removed Server Profile

--- policy.tf
+++ policy.tf
@@ -2,23 +2,6 @@ data "intersight_organization_organization" "org" {
     name = var.organization
 }
 
-
-
-resource "intersight_server_profile" "IMM-ESXi-02" {
-  name   = "IMM-ESXi-02"
-  action = "No-op"
-  target_platform = "FIAttached"
-
-  tags {
-    key   = "deployment_type"
-    value = "terraform"
-  }
-  organization {
-    object_type = "organization.Organization"
-    moid        = data.intersight_organization_organization.org.results[0].moid
-  }
-}
-
 resource "intersight_bios_policy" "Ibios_policy1" {
   name                                  = "IMM-Bios-Policy"
   description                           = "Created by Terraform"
