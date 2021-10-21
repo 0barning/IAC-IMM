44e9733723e13b78dcce565211446b96a4dbfb75
0barning <olaf.barning@outlook.com>
2021-10-21 10:08:51 +0200

    Added IQN Pool

--- policy.tf
+++ policy.tf
@@ -395,23 +395,23 @@ resource "intersight_macpool_pool" "intersight_macpool_poolB" {
 ############################
 # IQN Pool                 #
 ############################
-#
-#resource "intersight_iqnpool_pool" "iqnpool_pool1" {
-#  name             = "${var.env}_IQN_Pool"
-#  description      = "IQN Pool"
-#  assignment_order = "sequential"
-#  prefix           = var.iqnprefix
-#  iqn_suffix_blocks {
-#    object_type = "iqn.SuffixBlocks"
-#    suffix      = var.iqnsuffix
-#    from        = "6757"
-#    size        = "100"
-#  }
-#  organization {
-#    object_type = "organization.Organization"
-#    moid        = data.intersight_organization_organization.org.results[0].moid
-#  }
-#}
+
+resource "intersight_iqnpool_pool" "iqnpool_pool1" {
+  name             = "${var.env}_IQN_Pool"
+  description      = "IQN Pool"
+  assignment_order = "sequential"
+  prefix           = var.iqnprefix
+  iqn_suffix_blocks {
+    object_type = "iqn.SuffixBlocks"
+    suffix      = var.iqnsuffix
+    from        = "6757"
+    size        = "100"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.org.results[0].moid
+  }
+}
 
 ############################
 # VLAN ESXi MGMT                #
