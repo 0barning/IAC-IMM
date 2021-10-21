8d6968e1af0c27e0a2fe5842d859d9ec1e1dc03e
0barning <olaf.barning@outlook.com>
2021-10-21 10:05:41 +0200

    Added IP Pool

--- policy.tf
+++ policy.tf
@@ -315,25 +315,25 @@ resource "intersight_server_profile" "intersight_server_profile1" {
 # KVM IP Pool              #
 ############################
 
-#resource "intersight_ippool_pool" "intersight_ip_pool1" {
-#  name             = "${var.env}_KVM_IP_Pool"
-#  description      = "IP Pool for server KVM console"
-#  assignment_order = "sequential"
-#  ip_v4_config {
-#    gateway     = var.kvmdipgw
-#    netmask     = var.kvmdipmask
-#    primary_dns = var.primarydns
-#    secondary_dns = var.secondarydns
-#  }
-#  ip_v4_blocks {
-#    from        = var.kvmdipfrom
-#    to          = var.kvmdipto
-#  }
-#  organization {
-#    object_type = "organization.Organization"
-#    moid        = data.intersight_organization_organization.org.results[0].moid
-#  }
-#}
+resource "intersight_ippool_pool" "intersight_ip_pool1" {
+  name             = "${var.env}_KVM_IP_Pool"
+  description      = "IP Pool for server KVM console"
+  assignment_order = "sequential"
+  ip_v4_config {
+    gateway     = var.kvmdipgw
+    netmask     = var.kvmdipmask
+    primary_dns = var.primarydns
+    secondary_dns = var.secondarydns
+  }
+  ip_v4_blocks {
+    from        = var.kvmdipfrom
+    to          = var.kvmdipto
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.org.results[0].moid
+  }
+}
 
 ############################
 # UUID Pool                #
